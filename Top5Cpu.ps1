8f3ae1b7f85bf3313fefa78b6fdcc33c8ea6e3c0
Radossus <spool@seznam.cz>
2019-06-14 15:26:13 +0200

    top

--- Top5Cpu.ps1
+++ Top5Cpu.ps1
@@ -1,1 +1,1 @@
-Get-process | sort-object -Property CPU -Descending | Select-Object -First 5 -Property Name, CPU | ConvertTo-Html | Out-File C:\temp\Top5Cpu.html
+Get-process | sort-object -Property CPU -Descending | Select-Object -First 5 -Property Name, CPU | ConvertTo-Html 
