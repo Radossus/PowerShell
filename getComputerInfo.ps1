5e19202c54744bdaf7b706f6bef1910e820332c8
Radossus <spool@seznam.cz>
2019-06-26 09:59:15 +0200

    CHANGE

--- getComputerInfo.ps1
+++ getComputerInfo.ps1
@@ -42,3 +42,49 @@ function Get-InfoCompSystem {
     New-Object -TypeName PSObject -Property $props
 }
 
+function Get-InfoBadService {
+    [CmdletBinding()]
+    param(
+    [Parameter(Mandatory=$True)][string]$ComputerName
+    )
+    $svcs = Get-CimInstance -class Win32_Service -ComputerName $ComputerName `
+    -Filter "StartMode='Auto' AND State<>'Running'"
+    foreach ($svc in $svcs) {
+    $props = @{'ServiceName'=$svc.name;
+    'LogonAccount'=$svc.startname;
+    'DisplayName'=$svc.displayname}
+    New-Object -TypeName PSObject -Property $props
+    }
+}
+
+function Get-InfoProc {
+    [CmdletBinding()]
+    param(
+    [Parameter(Mandatory=$True)][string]$ComputerName
+    )
+    $procs = Get-CimInstance -class Win32_Process -ComputerName $ComputerName
+    foreach ($proc in $procs) {
+    $props = @{'ProcName'=$proc.name;
+    'Executable'=$proc.ExecutablePath}
+    New-Object -TypeName PSObject -Property $props
+    }
+}
+
+function Get-InfoNIC {
+    [CmdletBinding()]
+    param(
+    [Parameter(Mandatory=$True)][string]$ComputerName
+    )
+    $nics = Get-CimInstance -class Win32_NetworkAdapter -ComputerName $ComputerName `
+    -Filter "PhysicalAdapter=True"
+    foreach ($nic in $nics) {
+    $props = @{'NICName'=$nic.servicename;
+     'Speed'=$nic.speed / 1MB -as [int];
+     'Manufacturer'=$nic.manufacturer;
+     'MACAddress'=$nic.macaddress}
+    New-Object -TypeName PSObject -Property $props
+    }
+}
+
+
+
