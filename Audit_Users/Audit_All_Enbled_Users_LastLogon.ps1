ec733275b04311e1f34c7400753cdd7d285f2ca3
Radossus <spool@seznam.cz>
2019-07-08 13:43:01 +0200

    ADD

--- Audit_Users/Audit_All_Enbled_Users_LastLogon.ps1
+++ Audit_Users/Audit_All_Enbled_Users_LastLogon.ps1
@@ -17,6 +17,6 @@ $properties=@(
     'LastLogonTimeStamp'
     )
     
-    Get-ADUser -SearchBase "DC=GI,DC=TELE,DC=COM" -Filter {enabled -eq $true}  -Properties $properties | Select-Object -Property SamAccountName, cn, Enabled,DisplayName,employeeType,GivenName,Surname,mail,mobile,tnDomainName,@{N='tnEmploymentStartDate';E={$_.tnEmploymentStartDate[0]}},tnDomainLogin,@{N='tnEmploymentEndDate';E={$_.tnEmploymentEndDate[0]}},@{N='tnJobLegalEntity';E={$_.tnJobLegalEntity[0]}},@{N='tnManager';E={$_.tnManager[0]}},@{ n = "LastLogonTimeStamp"; e = { [datetime]::FromFileTime( $_.lastLogonTimestamp ) } } | Export-Csv Users_Export.csv     
+    Get-ADUser -SearchBase "DC=GI,DC=TELE,DC=COM" -Filter {(enabled -eq $true) -and (cn -like 'P*')}  -Properties $properties | Select-Object -Property SamAccountName, cn, Enabled,DisplayName,employeeType,GivenName,Surname,mail,mobile,tnDomainName,@{N='tnEmploymentStartDate';E={$_.tnEmploymentStartDate[0]}},tnDomainLogin,@{N='tnEmploymentEndDate';E={$_.tnEmploymentEndDate[0]}},@{N='tnJobLegalEntity';E={$_.tnJobLegalEntity[0]}},@{N='tnManager';E={$_.tnManager[0]}},@{ n = "LastLogonTimeStamp"; e = { [datetime]::FromFileTime( $_.lastLogonTimestamp ) } } | Export-Csv Users_Export.csv     
     
     
