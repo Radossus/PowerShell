b36b227d7cbeb0d63c640f8515715bdd803b1e54
Radossus <spool@seznam.cz>
2019-08-13 15:16:35 +0200

    add

---
+++ Untitled5.ps1
@@ -0,0 +1,2 @@
+﻿import-csv .\Users1.csv | Where-Object {$_ -match "WOWTESTDC1" -or $_ -match "tlnpgid01"} | Out-File UsersOut.csv -Verbose
+
---
+++ getFreeSpaceOfDisk.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.Synopsis
+This is brief comments
+.Description
+This is the long comments
+.Parameter ComputerName
+This is the name of remote compter
+.Example
+Connection to local computer
+getFreeSpaceOdDisk -computername localhost
+.Example
+Connection to remote computer
+getFreeSpaceOfDisk -computername DC
+#>
+param(
+    [Parameter(Mandatory=$true)]
+    [string[]]$computername = 'localhost'
+)
+
+Get-CimInstance -ComputerName $computername -ClassName Win32_LogicalDisk -Filter "DeviceID='c:'" | 
+    Select-Object -Property @{name="ComputerName";Expression={$_.PSComputerName}}, 
+        @{name="FreeGB";Expression={$_.freeSpace / 1gb -as [int]}}
